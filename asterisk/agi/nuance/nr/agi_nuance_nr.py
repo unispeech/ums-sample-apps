af0def4b903b873e0e3658ddb1b60f5bd80e3e7a
root <root@DESKTOP-H3USPV8>
2023-10-08 02:11:21 +0400

    nuance NRAAS api

---
+++ asterisk/agi/nuance/nr/agi_nuance_nr.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python3
+
+"""
+
+    Asterisk AGI NUANCE NR Application
+
+    This script interacts with NUANCE SR/SS API via UniMRCP server.
+
+    * Revision: 1
+    * Date: Oct 8, 2023
+    * Vendor: Universal Speech Solutions LLC
+
+"""
+
+ 
+
+import sys
+from asterisk.agi import *
+import random
+import xml.etree.ElementTree as ET
+VOICES=[
+    {"name":"en-US-JennyNeural","model":"neural","language":"en-US","gender":"FEMALE","sampleRateHz":24000},
+    {"name":"en-US-GuyNeural","model":"neural","language":"en-US","gender":"MALE","sampleRateHz":24000},
+    {"name":"en-US-AriaNeural","model":"neural","language":"en-US","gender":"FEMALE","sampleRateHz":24000},
+    {"name":"en-US-DavisNeural","model":"neural","language":"en-US","gender":"MALE","sampleRateHz":24000},
+    {"name":"en-US-ElizabethNeural","model":"neural","language":"en-US","gender":"FEMALE","sampleRateHz":24000},
+]
+
+class NuanceSRAPP:
+
+
+    """A class representing NUANCE SR  application"""
+
+    def __init__(self, options):
+
+        """Constructor"""
+        self.options = options
+        self.status = None
+        self.cause = None
+        self.prompt =" Welcome to Nuance speech transcription PLease say digits from 1-3 "
+
+ 
+    def detect_intent(self):
+        """Performs a streaming intent detection"""
+        self.compose_speech_options()
+        self.grammars = "%s,%s" % (self.compose_speech_grammar(), self.compose_dtmf_grammar())
+        self.synth_and_recog()
+
+
+
+    def synth_and_recog(self):
+
+        """This is an internal function which calls SynthAndRecog"""
+
+        if not self.prompt:
+            self.prompt = ' '
+        agi.verbose('got completion cause %s' % self.prompt)
+        args = "\\\"%s\\\",\\\"%s\\\",%s" % (
+            self.prompt, self.grammars, self.options)
+        agi.set_variable('RECOG_STATUS', '')
+        agi.set_variable('RECOG_COMPLETION_CAUSE', '')
+        self.action = None
+        agi.appexec('SynthandRecog', args)
+        self.status = agi.get_variable('RECOG_STATUS')
+        agi.verbose('got status %s' % self.status)
+
+        if self.status == 'OK':
+            self.cause = agi.get_variable('RECOG_COMPLETION_CAUSE')
+            agi.verbose('got completion cause %s' % self.cause)
+        else:
+            agi.verbose('recognition completed abnormally')
+
+ 
+
+    def compose_speech_grammar(self):
+
+        """Composes a built-in speech grammar"""
+
+        grammar = '/opt/unimrcp/data/grammar.xml'
+        separator = '?'
+
+        return grammar
+
+    def compose_speech_options(self):
+        """Composes speech options"""
+        index=random.randint(1,4)
+        separator = '&'
+        self.options = self.append_option_parameter(
+            self.options, "vn", VOICES[index]['name'], separator)
+        
+        self.options = self.append_option_parameter(
+            self.options, "vg", VOICES[index]['gender'], separator)        
+        
+        
+
+    def append_option_parameter(self, options, name, value, separator):
+        """Appends a name/value parameter to the specified grammar"""
+        options += "%s%s=%s" % (separator, name, value)
+        
+        return options
+
+    def compose_dtmf_grammar(self):
+
+        """Composes a built-in DTMF grammar"""
+
+        grammar = 'builtin:dtmf/digits'
+        separator = '?'
+
+        return grammar
+
+
+
+    def append_grammar_parameter(self, grammar, name, value, separator):
+
+        """Appends a name/value parameter to the specified grammar"""
+        grammar += "%s%s=%s" % (separator, name, value)
+        return grammar
+
+
+    def get_prompt(self):
+
+        """Retrieves prompt from the data returned by bot"""
+        prompt = agi.get_variable('RECOG_INSTANCE(0/0/SWI_literal)')
+        
+        # root = ET.fromstring(prompt)
+        # prompt=root.findall('.//instance/response_messages/text/text')
+        """Uncomment this line if your python version is 2.7"""
+        # if isinstance(prompt, str):
+           
+        #     prompt = unicode(prompt, 'utf-8')
+        agi.verbose('got prompt %s' % prompt)
+        return prompt
+ 
+
+    def run(self):
+
+        """Interacts with the caller in a loop until the dialog is complete"""      
+
+        if self.prompt:
+            processing = True
+
+        while processing:
+            self.detect_intent()
+            processing = True
+            if self.status == 'OK':
+                if self.cause == '000':
+                    self.prompt = "you said " +self.get_prompt()
+
+                elif self.cause != '001' and self.cause != '002':
+                    processing = False
+
+            elif self.cause != '001' and self.cause != '002':
+                processing = False
+
+ 
+
+        if not self.prompt:
+            self.prompt = 'Thank you. See you next time.'
+        agi.appexec('MRCPSynth', "\\\"%s\\\"" % self.prompt)
+
+ 
+
+agi = AGI()
+options = 'plt=1&b=1&sct=1000&sint=15000&nit=10000&p=ums'
+nuance_sr_app = NuanceSRAPP(options)
+nuance_sr_app.run()
+agi.verbose('exiting')
